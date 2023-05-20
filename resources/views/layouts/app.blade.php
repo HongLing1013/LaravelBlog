9193ec2a4aa2520ed68fd1fb6dd2191f52fc808c
hollie1013 <hollie1013chao@gmail.com>
2023-05-20 15:56:32 +0800

    修正無法刪除 +JQ 分類編輯頁面完成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="/assets/vendor/jquery/jquery-1.10.2.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
